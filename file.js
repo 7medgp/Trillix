69543baa2427372f789fea3d1525c3406c783b80
7medgp <gahmedhamza@gmail.com>
2023-04-09 23:40:31 +0000

    el home page kemlet ne9es ken fazet el modal wel chario

--- file.js
+++ file.js
@@ -4,6 +4,7 @@ let btntext=document.getElementById("btntext");
 let btnIcon=document.getElementById("btnIcon");
 let tile=document.getElementById("tile");
 let logo=document.getElementById("logo");
+let logo1=document.getElementById("logo1");
 btn.onclick=function(){
     document.body.classList.toggle("dark-theme");
     if( document.body.classList.contains("dark-theme")){
@@ -11,10 +12,24 @@ btn.onclick=function(){
         btntext.innerHTML="Light";
         tile.src ="img/tile.white.png";
         logo.src ="img/bl title.white.png";
+        logo1.src ="img/bl title.white.png";
     }else{
         btnIcon.class="bx bxs-moon";
         btntext.innerHTML="Dark";
         tile.src ="img/tile.png";
         logo.src ="img/bl title.png";
+        logo1.src ="img/bl title.png";
     }
+}
+/*menu humburger*/
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+/*kifech tna7i menu wa9tli tescrolli*/
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
